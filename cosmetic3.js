e501e0e372f5c28b072ef6ee9fd90aa393de66eb
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2023-03-22 09:58:41 +0900

    20230322-2

--- cosmetic3.js
+++ cosmetic3.js
@@ -1,24 +1,24 @@
 [
     {
-      id: 7,
-      title: "아워 비건 립밤 3.7g",
-      imgUrl: "/img/add7.jpg",
-      content: 9500,
-      price: 9500
+      "id": 37,
+      "title": "아워 비건 립밤 3.7g",
+      "imgUrl": "/img/add7.jpg",
+      "content": 9500,
+      "price": 9500
     },
     {
-      id: 8,
-      title: "떼르말(온천수) 워터 모이스처라이징 스킨 155ml",
-      imgUrl: "/img/add8.jpg",
-      content: 15000,
-      price: 11000
+      "id": 38,
+      "title": "떼르말(온천수) 워터 모이스처라이징 스킨 155ml",
+      "imgUrl": "/img/add8.jpg",
+      "content": 15000,
+      "price": 11000
     },
     {
-      id: 9,
-      title: "떼르말(온천수) 워터 모이스처라이징 로션 155ml",
-      imgUrl: "/img/add9.jpg",
-      content: 15000,
-      price: 11000
+      "id": 39,
+      "title": "떼르말(온천수) 워터 모이스처라이징 로션 155ml",
+      "imgUrl": "/img/add9.jpg",
+      "content": 15000,
+      "price": 11000
     }
 ]
 
