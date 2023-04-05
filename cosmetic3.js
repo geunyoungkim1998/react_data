88fc00095dda0b2aa010ebdea6ea8f68c5f935e6
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2023-04-05 16:05:57 +0900

    20230405-12

--- cosmetic3.js
+++ cosmetic3.js
@@ -2,21 +2,21 @@
     {
       "id": 37,
       "title": "아워 비건 립밤 3.7g",
-      "imgUrl": process.env.PUBLIC_URL+"/img/add7.jpg",
+      "imgUrl": 'https://geunyoungkim1998.github.io/react_data/add7.jpg',
       "content": 9500,
       "price": 9500
     },
     {
       "id": 38,
       "title": "떼르말(온천수) 워터 모이스처라이징 스킨 155ml",
-      "imgUrl": process.env.PUBLIC_URL+"/img/add8.jpg",
+      "imgUrl": 'https://geunyoungkim1998.github.io/react_data/add8.jpg',
       "content": 15000,
       "price": 11000
     },
     {
       "id": 39,
       "title": "떼르말(온천수) 워터 모이스처라이징 로션 155ml",
-      "imgUrl": process.env.PUBLIC_URL+"/img/add9.jpg",
+      "imgUrl": 'https://geunyoungkim1998.github.io/react_data/add9.jpg',
       "content": 15000,
       "price": 11000
     }
