1b966f281e37a1329afa19db1cfd2d07a32065a9
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2023-04-05 15:50:35 +0900

    20230405-5

--- cosmetic1.js
+++ cosmetic1.js
@@ -2,21 +2,21 @@ let cosmetic1=[
     {
       id: 31,
       title: "하루하나 히알루론산 젤리 마스크 1매",
-      imgUrl: process.env.PUBLIC_URL+"/img/add4.jpg",
+      imgUrl: "./img/add4.jpg",
       content: 3000,
       price: 3000
     },
     {
       id: 32,
       title: "트리트먼트 립 세럼 10ml",
-      imgUrl: process.env.PUBLIC_URL+"/img/add5.jpg",
+      imgUrl: "./img/add5.jpg",
       content: 10000,
       price: 6000
     },
     {
       id: 33,
       title: "아워 비건 어성초 98 시카 세럼 100ml",
-      imgUrl: process.env.PUBLIC_URL+"/img/add6.jpg",
+      imgUrl:"./img/add6.jpg",
       content: 20000,
       price: 20000
     }
