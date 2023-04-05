958eb38ab6ecfc1bff415e207cef26a7d5ac8b9a
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2023-04-05 15:46:22 +0900

    20230405-2

--- cosmetic2.js
+++ cosmetic2.js
@@ -2,21 +2,21 @@
     {
       "id": 34,
       "title": "허브그린 샴푸 리필 파우치 1100ml",
-      "imgUrl": process.env.PUBLIC_URL+"/img/add1.jpg",
+      "imgUrl": "/img/add1.jpg",
       "content": 35000,
       "price": 22000
     },
     {
       "id": 35,
       "title": "아워 비건 어성초 시카 토너 400ml",
-      "imgUrl": process.env.PUBLIC_URL+"/img/add2.jpg",
+      "imgUrl": "/img/add2.jpg",
       "content": 24000,
       "price": 24000
     },
     {
       "id": 36,
       "title": "퓨어 클렌징 티슈 80매",
-      "imgUrl": process.env.PUBLIC_URL+"/img/add3.jpg",
+      "imgUrl": "/img/add3.jpg",
       "content": 14000,
       "price": 7000
     }
